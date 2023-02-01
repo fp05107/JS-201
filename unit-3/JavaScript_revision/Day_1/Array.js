a8672e32c57961d757ff65029b23a0d87c046786
subham <andharalauri@gmail.com>
2023-02-01 13:08:36 +0530

    added

---
+++ unit-3/JavaScript_revision/Day_1/Array.js
@@ -0,0 +1,37 @@
+/*  declaration
+    accessing items
+    assignment
+    Array methods
+    2D array
+    3D array
+    forEach, map, reduce, filter callbacks
+*/
+
+let arr1 = [];
+let arr2 = new Array(10)
+console.log(arr2);
+
+/*
+Array.map(): Creates a new array by calling a provided function on each element in the original array.
+Array.filter(): Creates a new array with elements that pass a certain test.
+Array.reduce(): Reduces an array to a single value by applying a callback function to each element.
+Array.forEach(): Calls a provided function on each element in an array.
+Array.sort(): Sorts the elements in an array.
+Array.reverse(): Reverses the order of the elements in an array.
+Array.concat(): Combines two or more arrays into a new array.
+Array.slice(): Extracts a portion of an array and returns a new array.
+Array.splice(): Adds or removes elements from an array.
+Array.indexOf(): Searches for an element in an array and returns its index, or -1 if the element is not found.
+Array.includes(): Determines whether an array includes a certain element.
+Array.join(): Joins all elements of an array into a string.
+*/
+
+let arr3 = [1,2,3,4,5];
+let arr4 = [1,2,3];
+let arr5 = arr3.concat(arr4);
+console.log(arr5);
+
+
+
+
+
--- unit-3/JavaScript_revision/Day_1/HigherOrderFunctions.js
+++ unit-3/JavaScript_revision/Day_1/HigherOrderFunctions.js
@@ -1,6 +1,25 @@
 
-/* =====Reduce====*/
-const numbers = [1, 2, 3, 4, 5];
-const sum = numbers.reduce((acc, current) => acc + current, 0);
-console.log(sum); 
+/*=====Reduce====*/
 
+const numbers1 = [1, 2, 3, 4, 5];
+const sum = numbers1.reduce((acc, current) => acc + current, 0);
+//console.log(sum); 
+
+/*======filter=======*/
+
+const numbers2 = [1, 2, 3, 4, 5];
+const evenNumbers = numbers2.filter(num => num % 2 == 0);
+//console.log(evenNumbers);
+
+/*======map=======*/
+
+let number3 = [1, 2, 3, 4, 5];
+let mappedNumbers = number3.map(e => e = e+10);
+//console.log(mappedNumbers);
+
+/*=======forEach======= */
+
+let number4 = [1,2,3,4,5];
+let sum2 = 0;
+number4.forEach(e => sum2=sum2+e);
+console.log(sum2);
